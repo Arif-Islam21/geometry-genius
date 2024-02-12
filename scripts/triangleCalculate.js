6cf1e2c0cc919b5a80c60475bf8f41bae6aac66c
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-12 21:28:08 +0600

    found the base and height of triangle

--- scripts/triangleCalculate.js
+++ scripts/triangleCalculate.js
@@ -1,3 +1,11 @@
 function triangleArea() {
-  console.log("triangle button clicked");
+  const triangleBaseInput = document.getElementById("triangle-base");
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  // console.log(base);
+
+  const triangleHeightInput = document.getElementById("triangle-height");
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  // console.log(height);
 }
