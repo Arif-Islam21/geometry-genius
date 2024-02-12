e9d51c55af5e34b0746ba9d64e31d788982e6709
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-12 23:31:23 +0600

    discovered a shortcut and effective way to declare function

---
+++ scripts/commonFunction.js
@@ -0,0 +1,8 @@
+function input(id) {
+  const input = document.getElementById(id);
+  const text = input.value;
+  const length = parseFloat(text);
+  return length;
+}
+
+// getting the common way
--- scripts/rectangleCalculate.js
+++ scripts/rectangleCalculate.js
@@ -2,15 +2,8 @@ document
   .getElementById("rectangle-calculate-btn")
   .addEventListener("click", function () {
     // GET WIDTH OF THE RECTANGLE
-    const rectangleWidthInput = document.getElementById("rectangle-width");
-    const rectangleWidthText = rectangleWidthInput.value;
-    const width = parseFloat(rectangleWidthText);
-    // console.log(width);
-    // GET LENGTH OF THE RECTANGLE
-    const rectLengthInput = document.getElementById("rectangle-length");
-    const rectangleText = rectLengthInput.value;
-    const length = parseFloat(rectangleText);
-    // console.log(length);
+    input("rectangle-width");
+    input("rectangle-length");
 
     const area = length * width;
 
